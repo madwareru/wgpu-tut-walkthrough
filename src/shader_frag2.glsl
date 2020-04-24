cebf7da30089df113f6e6c61dcb18f8677c61446
madwareru <madware.ru@gmail.com>
2020-04-24 23:41:02 +0300

    Made vertices in a switched pipeline colored based on their positions (challenge in lesson 3 is complete)

--- src/shader_frag2.glsl
+++ src/shader_frag2.glsl
@@ -1,7 +1,8 @@
 #version 450
 
+layout(location=0) in vec4 v_color;
 layout(location=0) out vec4 f_color;
 
 void main() {
-    f_color = vec4(0.7, 0.2, 0.1, 0.1);
+    f_color = v_color;
 }
--- src/shader_vert2.glsl
+++ src/shader_vert2.glsl
@@ -1,5 +1,7 @@
 #version 450
 
+layout(location=0) out vec4 v_color;
+
 const vec2 positions[3] = vec2[3](
     vec2( 0.0, -0.5),
     vec2(-0.5,  0.5),
@@ -7,5 +9,6 @@ const vec2 positions[3] = vec2[3](
 );
 
 void main() {
+    v_color = vec4((vec2(1.0, 1.0) + positions[gl_VertexIndex] * 2.0) / 2.0, 0.0, 1.0);
     gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
 }
