b4474def5232d6e9a601160060eb1ffa01de6729
madwareru <madware.ru@gmail.com>
2020-05-02 17:17:51 +0300

    So be uniforms!

--- src/main.rs
+++ src/main.rs
@@ -38,9 +38,9 @@ impl VertexData {
 }
 
 const TRIANGLE_VERTICES: &[VertexData] = &[
-    VertexData{ position: [ 0.0, -0.5, 0.0], uv: [0.5, 0.0]},
-    VertexData{ position: [-0.5,  0.5, 0.0], uv: [0.0, 1.0]},
-    VertexData{ position: [ 0.5,  0.5, 0.0], uv: [1.0, 1.0]},
+    VertexData{ position: [ 0.0,  0.5, 0.0], uv: [0.5, 0.0]},
+    VertexData{ position: [ 0.5, -0.5, 0.0], uv: [1.0, 1.0]},
+    VertexData{ position: [-0.5, -0.5, 0.0], uv: [0.0, 1.0]},
 ];
 
 const TRIANGLE_INDICES: &[u16] = &[
@@ -48,16 +48,61 @@ const TRIANGLE_INDICES: &[u16] = &[
 ];
 
 const QUAD_VERTICES: &[VertexData] = &[
-    VertexData{ position: [-0.5, -0.5, 0.0], uv: [0.0, 0.0]},
-    VertexData{ position: [-0.5,  0.5, 0.0], uv: [0.0, 1.0]},
-    VertexData{ position: [ 0.5,  0.5, 0.0], uv: [1.0, 1.0]},
-    VertexData{ position: [ 0.5, -0.5, 0.0], uv: [1.0, 0.0]},
+    VertexData{ position: [-0.5,  0.5, 0.0], uv: [0.0, 0.0]},
+    VertexData{ position: [-0.5, -0.5, 0.0], uv: [0.0, 1.0]},
+    VertexData{ position: [ 0.5, -0.5, 0.0], uv: [1.0, 1.0]},
+    VertexData{ position: [ 0.5,  0.5, 0.0], uv: [1.0, 0.0]},
 ];
 
 const QUAD_INDICES: &[u16] = &[
-    0, 1, 2,
-    2, 3, 0,
+    0, 2, 1,
+    2, 0, 3,
 ];
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0,  0.0, 0.0, 0.0,
+    0.0, -1.0, 0.0, 0.0,
+    0.0,  0.0, 0.5, 0.0,
+    0.0,  0.0, 0.5, 1.0
+);
+
+struct Camera{
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+
+impl Camera {
+    fn to_view_proj(&self) -> cgmath::Matrix4<f32> {
+        let view = cgmath::Matrix4::look_at(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+        OPENGL_TO_WGPU_MATRIX * proj * view
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct Uniforms {
+    view_proj: cgmath::Matrix4<f32>
+}
+
+impl Uniforms {
+    fn new() -> Self {
+        use cgmath::SquareMatrix;
+        Self {
+            view_proj: cgmath::Matrix4::identity()
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.to_view_proj();
+    }
+}
 
 struct Mesh {
     vertex_buffer: wgpu::Buffer,
@@ -77,13 +122,17 @@ struct Texture {
 }
 
 struct UserData {
+    camera: Camera,
+    uniforms: Uniforms,
+    uniform_buffer: wgpu::Buffer,
+    uniform_bind_group: wgpu::BindGroup,
     clear_color: wgpu::Color,
     triangle_mesh: Mesh,
     quad_mesh: Mesh,
     render_pipeline: wgpu::RenderPipeline,
     mode_switched: bool,
     necromancer_texture: Texture,
-    mage_texture: Texture
+    mage_texture: Texture,
 }
 
 struct MainState {
@@ -102,7 +151,8 @@ fn create_render_pipeline_from_shaders(
     device: &wgpu::Device,
     vert_src: &'static str,
     frag_src: &'static str,
-    bind_group_layout: &wgpu::BindGroupLayout
+    texture_bind_group_layout: &wgpu::BindGroupLayout,
+    uniforms_bind_group_layout: &wgpu::BindGroupLayout
 ) -> wgpu::RenderPipeline {
     let vs_spirv = glsl_to_spirv::compile(vert_src, glsl_to_spirv::ShaderType::Vertex).unwrap();
     let fs_spirv = glsl_to_spirv::compile(frag_src, glsl_to_spirv::ShaderType::Fragment).unwrap();
@@ -114,7 +164,10 @@ fn create_render_pipeline_from_shaders(
     let fs_module = device.create_shader_module(&fs_data);
 
     let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor{
-        bind_group_layouts: &[&bind_group_layout]
+        bind_group_layouts: &[
+            &texture_bind_group_layout,
+            &uniforms_bind_group_layout
+        ]
     });
 
     device.create_render_pipeline(&wgpu::RenderPipelineDescriptor{
@@ -139,7 +192,11 @@ fn create_render_pipeline_from_shaders(
         color_states: &[
             wgpu::ColorStateDescriptor {
                 format: wgpu::TextureFormat::Bgra8UnormSrgb,
-                color_blend: wgpu::BlendDescriptor::REPLACE,
+                color_blend: wgpu::BlendDescriptor{
+                    src_factor: wgpu::BlendFactor::SrcAlpha,
+                    dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
+                    operation: wgpu::BlendOperation::Add
+                },
                 alpha_blend: wgpu::BlendDescriptor::REPLACE,
                 write_mask: wgpu::ColorWrite::ALL
             }
@@ -333,12 +390,66 @@ impl MainState {
             bind_group: mage_bind_group
         };
 
-        let render_pipeline = create_render_pipeline_from_shaders(&device, vs_src, fs_src, &texture_bind_group_layout);
+        let camera = Camera {
+            eye: (0.0, 1.0, -2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: swap_chain_desc.width as f32 / swap_chain_desc.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0
+        };
+
+        let mut uniforms = Uniforms::new();
+        uniforms.update_view_proj(&camera);
+
+        let uniform_buffer = device
+            .create_buffer_mapped(1, wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST)
+            .fill_from_slice(&[uniforms]);
+
+        let uniform_bind_group_layout = device
+            .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                bindings: &[
+                    wgpu::BindGroupLayoutBinding {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::VERTEX,
+                        ty: wgpu::BindingType::UniformBuffer {
+                            dynamic: false
+                        }
+                    }
+                ]
+            });
+
+        let uniform_bind_group = device
+            .create_bind_group(&wgpu::BindGroupDescriptor{
+                layout: &uniform_bind_group_layout,
+                bindings: &[
+                    wgpu::Binding {
+                        binding: 0,
+                        resource: wgpu::BindingResource::Buffer {
+                            buffer: &uniform_buffer,
+                            range: 0..std::mem::size_of_val(&uniforms) as wgpu::BufferAddress
+                        }
+                    }
+                ]
+            });
+
+        let render_pipeline = create_render_pipeline_from_shaders(
+            &device,
+            vs_src,
+            fs_src,
+            &texture_bind_group_layout,
+            &uniform_bind_group_layout
+        );
 
         let triangle_mesh = create_mesh(&device, TRIANGLE_VERTICES, TRIANGLE_INDICES);
         let quad_mesh = create_mesh(&device, QUAD_VERTICES, QUAD_INDICES);
 
         let user_data = UserData {
+            camera,
+            uniforms,
+            uniform_buffer,
+            uniform_bind_group,
             clear_color: wgpu::Color{r:0.1, g: 0.2, b: 0.3, a: 1.0},
             render_pipeline,
             mode_switched: false,
@@ -425,6 +536,7 @@ impl MainState {
             render_pass.set_vertex_buffers(0, &[(&mesh.vertex_buffer, 0)]);
             render_pass.set_index_buffer(&mesh.index_buffer, 0);
             render_pass.set_bind_group(0, &texture.bind_group, &[]);
+            render_pass.set_bind_group(1, &self.user_data.uniform_bind_group, &[]);
             render_pass.draw_indexed(0..mesh.num_indices, 0, 0..1);
         }
 
